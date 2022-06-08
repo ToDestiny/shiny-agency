5d4dd2fedfba02e62e27ff71ffc3c74e220beba0
Anthony COLAS <anthonycolas.france@gmail.com>
2022-06-08 19:38:46 +0200

    CSS for main pages - OK

--- src/index.jsx
+++ src/index.jsx
@@ -8,11 +8,23 @@ import Freelances from './pages/Freelances/index'
 import Header from './components/Header/index'
 import Error from './components/Error/index'
 import { Switch } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    div {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+
+    body {
+      margin: 0;
+    }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path="/">
